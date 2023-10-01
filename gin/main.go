f120302da44b2dda15427371b0834e8bc7c518bc
MrLQQ <2802350194@qq.com>
2023-10-01 19:53:12 +0800

    Gin入门，路由练习

---
+++ gin/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	server := gin.Default()
+	// 静态路由
+	server.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello, world")
+	})
+
+	server.POST("/login", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello,login")
+	})
+
+	// 参数路由
+	// 路由参数
+	server.GET("/users/:name", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		ctx.String(http.StatusOK, "hello,"+name)
+	})
+	//查询参数
+	server.GET("/order", func(ctx *gin.Context) {
+		id := ctx.Query("id")
+		ctx.String(http.StatusOK, "订单ID为,"+id)
+	})
+
+	// 通配符路由
+	server.GET("/views/*.html", func(ctx *gin.Context) {
+		view := ctx.Param(".html")
+		ctx.String(http.StatusOK, "您正在浏览"+view)
+	})
+
+	// 如果不传参数，会监听8080端口
+	server.Run(":8080")
+}
