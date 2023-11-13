a799dc6b081cab0bb156d70c05e2f257ed2dad5c
MrLQQ <2802350194@qq.com>
2023-11-13 20:59:23 +0800

    配置模块入门、使用 viper 读取本地配置

--- webook/ioc/db.go
+++ webook/ioc/db.go
@@ -1,15 +1,26 @@
 package ioc
 
 import (
-	"basic-go/webook/config"
 	"basic-go/webook/internal/repository/dao"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN), &gorm.Config{
+	type Config struct {
+		DSN string `yaml:"DSN"`
+	}
+	// 提前给默认值
+	//var cfg = Config{DSN: "root:root@tcp(localhost:13316)/webook"}
+	var cfg Config
+	err := viper.UnmarshalKey("db", &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(mysql.Open(cfg.DSN), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
--- webook/main.go
+++ webook/main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"log"
 	"net/http"
 	//"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	initViperV1()
 	server := InitWebServer()
 	server.GET("/hello", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "hello,启动成功")
 	})
 	server.Run(":8080")
+}
+
+func initViper() {
+	viper.SetConfigName("dev")
+	viper.SetConfigType("yaml")
+	// 当前工作目录的config目录
+	viper.AddConfigPath("config")
+	// 读取配置
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	val := viper.Get("test.key")
+	log.Println(val)
+}
+
+func initViperV1() {
+	cfalg := pflag.String("config", "config/config.yaml", "配置文件路径")
+	// 这一步之后 cfalg才有值
+	pflag.Parse()
+
+	//viper.Set("db.dsn", "root:root@tcp(localhost:13316)/webook")
+	viper.SetConfigFile(*cfalg)
+	// 读取配置
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	val := viper.Get("test.key")
+	log.Println(val)
 }
