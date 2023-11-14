60b506a2266081cdefbb5d101bcbc2dfd714d1a9
MrLQQ <2802350194@qq.com>
2023-11-14 14:59:24 +0800

    远程配置中心etcd

--- webook/main.go
+++ webook/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"log"
 	"net/http"
+	"time"
+
 	//"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
+	_ "github.com/spf13/viper/remote"
 )
 
 func main() {
 	initViperV1()
+	//initViperWatch()
 	server := InitWebServer()
 	server.GET("/hello", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "hello,启动成功")
@@ -32,13 +37,18 @@ func initViper() {
 	log.Println(val)
 }
 
-func initViperV1() {
-	cfalg := pflag.String("config", "config/config.yaml", "配置文件路径")
+// initViperWatch 监听配置变更
+func initViperWatch() {
+	cfile := pflag.String("config", "config/config.yaml", "配置文件路径")
 	// 这一步之后 cfalg才有值
 	pflag.Parse()
 
 	//viper.Set("db.dsn", "root:root@tcp(localhost:13316)/webook")
-	viper.SetConfigFile(*cfalg)
+	viper.SetConfigFile(*cfile)
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		log.Println(viper.GetString("test.key"))
+	})
 	// 读取配置
 	err := viper.ReadInConfig()
 	if err != nil {
@@ -46,4 +56,45 @@ func initViperV1() {
 	}
 	val := viper.Get("test.key")
 	log.Println(val)
+}
+
+func initViperV1() {
+	cfile := pflag.String("config", "config/config.yaml", "配置文件路径")
+	// 这一步之后 cfalg才有值
+	pflag.Parse()
+
+	//viper.Set("db.dsn", "root:root@tcp(localhost:13316)/webook")
+	viper.SetConfigFile(*cfile)
+	// 读取配置
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	val := viper.Get("test.key")
+	log.Println(val)
+}
+
+func initViperRemote() {
+	err := viper.AddRemoteProvider("etcd3", "127.0.0.1:12379", "/webook")
+	if err != nil {
+		panic(err)
+	}
+	viper.SetConfigType("yaml")
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		log.Println("远程配置中心发生变更")
+	})
+	err = viper.ReadRemoteConfig()
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			err = viper.WatchRemoteConfig()
+			if err != nil {
+				panic(err)
+			}
+			log.Println("watch", viper.GetString("test.key"))
+			time.Sleep(time.Second * 3)
+		}
+	}()
 }
