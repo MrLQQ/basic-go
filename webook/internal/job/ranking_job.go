51622b9ffb66ca91a3f069e1d9862044efe16cf0
MrLQQ <2802351094@qq.com>
2024-11-26 15:22:17 +0800

    分布式锁实现：控制任务只能在一个节点上运行

--- webook/internal/job/ranking_job.go
+++ webook/internal/job/ranking_job.go
@@ -3,16 +3,27 @@ package job
 import (
 	"context"
 	"gitee.com/geekbang/basic-go/webook/internal/service"
+	"gitee.com/geekbang/basic-go/webook/pkg/logger"
+	rlock "github.com/gotomicro/redis-lock"
 	"time"
 )
 
 type RankingJob struct {
 	svc     service.RankingService
+	l       logger.LoggerV1
 	timeout time.Duration
+	client  *rlock.Client
+	key     string
 }
 
-func NewRankingJob(svc service.RankingService, timeout time.Duration) *RankingJob {
-	return &RankingJob{svc: svc, timeout: timeout}
+func NewRankingJob(
+	svc service.RankingService,
+	l logger.LoggerV1,
+	timeout time.Duration) *RankingJob {
+	return &RankingJob{svc: svc,
+		key:     "job:ranking",
+		l:       l,
+		timeout: timeout}
 }
 
 func (r *RankingJob) Name() string {
@@ -20,7 +31,27 @@ func (r *RankingJob) Name() string {
 }
 
 func (r *RankingJob) Run() error {
-	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*4)
+	defer cancel()
+	lock, err := r.client.Lock(ctx, r.key, r.timeout,
+		&rlock.FixIntervalRetry{
+			Interval: time.Millisecond * 100,
+			Max:      3,
+			// 重试的超时
+		}, time.Second)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		// 解锁
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		er := lock.Unlock(ctx)
+		if er != nil {
+			r.l.Error("ranking job释放分布锁失败", logger.Error(er))
+		}
+	}()
+	ctx, cancel = context.WithTimeout(context.Background(), r.timeout)
 	defer cancel()
 	return r.svc.TopN(ctx)
 }
