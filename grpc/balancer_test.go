12633819f9a013e1e407ee99f71ef77a49e09c4a
MrLQQ <2802351094@qq.com>
2024-12-05 15:23:16 +0800

    grpc负载均衡

--- grpc/balancer_test.go
+++ grpc/balancer_test.go
@@ -5,10 +5,13 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	etcdv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/naming/endpoints"
 	"go.etcd.io/etcd/client/v3/naming/resolver"
 	"google.golang.org/grpc"
 	_ "google.golang.org/grpc/balancer/weightedroundrobin"
 	"google.golang.org/grpc/credentials/insecure"
+	"net"
+	"testing"
 	"time"
 )
 
@@ -79,4 +82,59 @@ func (s *BalancerTestSuite) TestClient() {
 		require.NoError(t, err)
 		t.Log(resp.User)
 	}
+}
+
+func (s *BalancerTestSuite) TestServer() {
+	go func() {
+		s.startServer(":8090")
+	}()
+	s.startServer("8091")
+}
+
+func (s *BalancerTestSuite) startServer(addr string) {
+	t := s.T()
+	em, err := endpoints.NewManager(s.cli, "service/user")
+	require.NoError(t, err)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	addr = "127.0.0.1" + addr
+	key := "service/user/" + addr
+	l, err := net.Listen("tcp", addr)
+	require.NoError(s.T(), err)
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// 租期
+	var ttl int64 = 5
+	leaseResp, err := s.cli.Grant(ctx, ttl)
+	require.NoError(t, err)
+
+	err = em.AddEndpoint(ctx, key, endpoints.Endpoint{
+		// 定位信息，客户端怎么连你
+		Addr: addr,
+	}, etcdv3.WithLease(leaseResp.ID))
+	require.NoError(t, err)
+	kaCtx, kaCancel := context.WithCancel(context.Background())
+	go func() {
+		_, err1 := s.cli.KeepAlive(kaCtx, leaseResp.ID)
+		require.NoError(t, err1)
+		//for kaResp := range ch {
+		//t.Log(kaResp.String())
+		//}
+	}()
+
+	server := grpc.NewServer()
+	RegisterUserServiceServer(server, &Server{})
+	server.Serve(l)
+	kaCancel()
+	err = em.DeleteEndpoint(ctx, key)
+	if err != nil {
+		t.Log(err)
+	}
+	server.GracefulStop()
+	//s.cli.Close()
+}
+
+func TestBalancer(t *testing.T) {
+	suite.Run(t, new(BalancerTestSuite))
 }
