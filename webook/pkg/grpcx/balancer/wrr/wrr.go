dd5906b7634bf52e68a5e924ed54d24a1b620cc3
MrLQQ <2802351094@qq.com>
2024-12-06 10:19:28 +0800

    动态调整权重

--- webook/pkg/grpcx/balancer/wrr/wrr.go
+++ webook/pkg/grpcx/balancer/wrr/wrr.go
@@ -3,6 +3,7 @@ package wrr
 import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
+	"math"
 	"sync"
 )
 
@@ -45,8 +46,8 @@ type Picker struct {
 
 // Pick 实现加权平滑轮询
 func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
-	p.lock.Lock()
-	defer p.lock.Unlock()
+	//p.lock.Lock()
+	//defer p.lock.Unlock()
 	// 判断是否为空
 	if len(p.conns) == 0 {
 		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
@@ -55,6 +56,7 @@ func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	var total int
 	var maxCC *weightConn
 	for _, c := range p.conns {
+		c.mutex.Lock()
 		// 1.计算所有的权重之和
 		total += c.weight
 		// 2.每个节点当前权重和初始权重相加得到最新的当前权重
@@ -63,20 +65,51 @@ func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 		if maxCC == nil || maxCC.currentWeight < c.currentWeight {
 			maxCC = c
 		}
+		c.mutex.Unlock()
 	}
+	maxCC.mutex.Lock()
 	// 4.成功后将选中节点的当前权重减去总权重
 	maxCC.currentWeight -= total
+	maxCC.mutex.Unlock()
 
 	return balancer.PickResult{
 		SubConn: maxCC.SubConn,
 		// 这个是调用回调接口
 		Done: func(info balancer.DoneInfo) {
 			// 要在这里进一步调整weight/currentWeight
+			// failover 在这里做文章
+			// 根据调用结果的具体错误信息进行容错
+			// 1.如果要是触发了限流
+			// 1.1 你可以考虑直接挪走这个节点，后面再挪回来
+			// 1.2 你可考虑直接将 weight/currentWight 调整到极低
+			// 2.如果触发了熔断 可以优先考虑1.1的方法
+			// 3.降级呢 可以优先考虑1.1的方法
+
+			//------------简单实现动态调整权重---------------
+			maxCC.mutex.Lock()
+			defer maxCC.mutex.Unlock()
+			// 权重最小下限校验
+			if info.Err != nil && maxCC.weight == 1 {
+				return
+			}
+			// 权重最大上限校验
+			if info.Err == nil && maxCC.weight == math.MaxUint32 {
+				return
+			}
+
+			// 动态调整权重，调用成功提升权重，调用失败降低权重
+			if info.Err != nil {
+				maxCC.weight--
+			} else {
+				maxCC.weight++
+			}
 		},
 	}, nil
 }
 
 type weightConn struct {
+	mutex sync.Mutex
+
 	balancer.SubConn
 	weight        int
 	currentWeight int
